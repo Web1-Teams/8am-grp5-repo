45dd33007d0ace83ab8ed5ad4defbc45eb3e4337
ShaymaTuriaqe <s12324759@stu.najah.edu>
2025-01-04 02:59:01 -0800

    shaymaReact

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Form from './components/Form'
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import Card from './componentsShayma/Card';
 
 
 
 function App() {
   return (
- 
+     <>
+   <Card/>
   <Form/>
-  
  
+ 
+
+ </>
   );
 }
 
