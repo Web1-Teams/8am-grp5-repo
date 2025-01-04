19c92f0ef36a98132e7802ede364bd3aebb8d68b
WijdanDela <s12323655@stu.najah.edu>
2025-01-04 17:02:42 +0200

    ..

---
+++ src/WijdanBlog/BlogPost3.js
@@ -0,0 +1,10 @@
+const BlogPost3 = {
+    imgSrc: "./2435696ece8077a165e79120951d9db0.png.webp",
+    imgAlt: "Seas accumsan nulla nec lacus ultricies placerat.",
+    dateTime: "2022-03-24",
+    title: "أسئلة شائعة",
+    description: "كيف أتخلص من رائحة الفم الكريهة؟ متى أحتاج إلى زراعة الأسنان؟ ما هي أفضل الطرق لتخفيف آلام الأسنان في المنزل؟",
+    link: "https://mawdoo3.com/%D8%B7%D8%B1%D9%82_%D8%A5%D8%B2%D8%A7%D9%84%D8%A9_%D8%B1%D8%A7%D8%A6%D8%AD%D8%A9_%D8%A7%D9%84%D9%81%D9%85_%D8%A7%D9%84%D9%83%D8%B1%D9%8A%D9%87%D8%A9_%D9%86%D9%87%D8%A7%D8%A6%D9%8A%D8%A7%D9%8B"
+};
+
+export default BlogPost3;
---
+++ src/WijdanBlog/BlogPost6.js
@@ -0,0 +1,10 @@
+const BlogPost6 = {
+    imgSrc: "./ee531b93-5d9f-40db-82ce-df8075830964.webp",
+    imgAlt: "Seas accumsan nulla nec lacus ultricies placerat.",
+    dateTime: "2017-03-28",
+    title: "أسئلة شائعة",
+    description: "أسباب حساسية الأسنان؟",
+    link: "https://mawdoo3.com/أسباب_حساسية_الأسنان"
+};
+
+export default BlogPost6
