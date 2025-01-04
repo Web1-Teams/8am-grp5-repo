2c8cb1a52eac6af78df26508f5c4c74bf9156d31
AbdulqaderShtaya <S12112199@stu.najah.edu>
2025-01-04 21:49:09 +0200

    Add files via upload

---
+++ src/About-page-abdulqader/MedicalTeam.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import './Stylemedicalteam.css'; 
+
+const MedicalTeam = () => {
+  return (
+    <section id="team-intro">
+      <h2>فريقنا الطبي</h2>
+      <p>
+        يتكون فريقنا الطبي من أطباء متميزين بخبرة عالية ومهارات استثنائية، لضمان حصولك على ابتسامة مثالية وصحة فموية ممتازة.
+      </p>
+      <img src="midicalteamimg.jpg" alt="Our Medical Team" />
+    </section>
+  );
+};
+
+export default MedicalTeam
